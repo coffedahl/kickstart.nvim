3041ea5a7a5dfad85a2b2f86790b475de950bb87
Christoffer Dahl <coffedahl@hotmail.com>
2024-07-23 18:24:31 +0200

    coffedahls setup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  vim.keymap.set('n', '<leader>q', vim.cmd.q),
+  vim.keymap.set('n', '<leader>ö', vim.cmd.Ex),
+  vim.keymap.set('n', '<leader>w', vim.cmd.w),
+  vim.keymap.set('n', '<leader>i', vim.cmd.cc),
+}
