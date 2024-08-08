8bd24b9c9a941fa1526f9b329b59438445370d83
Christoffer Dahl <coffedahl@hotmail.com>
2024-08-08 23:54:02 +0200

    Neotree takeover and relative numbers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,13 +2,21 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Neotree instead of netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- Tabs to 2 spaces
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
-
+-- Relative line numbers
+vim.wo.relativenumber = true
 return {
+  -- theme
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  -- keybinds
   vim.keymap.set('n', '<leader>q', vim.cmd.q),
   vim.keymap.set('n', '<leader>ö', vim.cmd.Ex),
   vim.keymap.set('n', '<leader>w', vim.cmd.w),
--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -7,4 +7,12 @@ return {
     'MunifTanjim/nui.nvim',
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  config = function()
+    require('neo-tree').setup {
+      filters = {
+        dotfiles = true,
+      },
+      lazy = false,
+    }
+  end,
 }
